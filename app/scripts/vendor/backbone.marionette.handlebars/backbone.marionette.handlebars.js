158a33f21d686ec30a2f3deafcc810e178c273fb
hukuuu <hukuuu@gmail.com>
2013-03-13 23:34:03 +0200

    a

--- app/scripts/vendor/backbone.marionette.handlebars/backbone.marionette.handlebars.js
+++ app/scripts/vendor/backbone.marionette.handlebars/backbone.marionette.handlebars.js
@@ -23,7 +23,7 @@ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
 DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE.*/
-console.log('!!!!!!!!!!!!!');
+
 (function (root, define, require, exports, module, factory, undef) {
     'use strict';
     if (typeof exports === 'object') {
